551756f107084886063d6bc7ac1911ad516e4691
emilly94 <emillysporto.94@gmail.com>
2021-12-22 17:34:36 -0300

    validacao do campo em branco

--- Projeto-Javascript/js/script.js
+++ Projeto-Javascript/js/script.js
@@ -1,25 +1,62 @@
+
 document.addEventListener('submit', function(event){
   event.preventDefault();
 
-  var a = parseFloat(document.getElementById('a').value);
-  var b = parseFloat(document.getElementById('b').value);
-  var c = parseFloat(document.getElementById('c').value);
-
-  const media = (a+b+c)/3;
-  console.log(a);
   if(document.getElementById('a').value == NaN || document.getElementById('a').value == ''){
-    document.getElementById('a').style.borderColor = "#faa";
-    alert('Por favor preencha o campo em vermelho');
+    //document.getElementById('a').style.borderColor = "#faa";
+    document.getElementById('msg').innerHTML = "Preencha o campo";
+    setTimeout(function(){ 
+      var msg = document.getElementById("msg");
+      msg.parentNode.removeChild(msg);   
+  }, 4000);
+    
   }else if(document.getElementById('b').value == NaN || document.getElementById('b').value == ''){
-    document.getElementById('b').style.borderColor = "#faa";
-    alert('Por favor preencha do campo em vermelho');
+    document.getElementById('msg').innerHTML = "Preencha o campo";
+    setTimeout(function(){ 
+      var msg = document.getElementById("msg");
+      msg.parentNode.removeChild(msg);   
+  }, 4000);
   }else if(document.getElementById('c').value == NaN || document.getElementById('c').value == ''){
-    document.getElementById('c').style.borderColor = "#faa";
-    alert('Por favor preencha o campo em vermelho');
+    document.getElementById('msg').innerHTML = "Preencha o campo";
+    setTimeout(function(){ 
+      var msg = document.getElementById("msg");
+      msg.parentNode.removeChild(msg);   
+  }, 4000);
   }else{
-    document.getElementById('resultado').innerHTML = "Resultado da média é: " + media;
+    //document.getElementById('resultado').innerHTML = "Resultado da média é: " + media;
+   dadosMedia();
   }
 })
+
+//inserindo dados em uma tabela
+function dadosMedia(){
+   
+   var nome   = document.getElementById('aluno').value;
+   var nota1 = parseFloat(document.getElementById('a').value);
+   var nota2 = parseFloat(document.getElementById('b').value);
+   var nota3 = parseFloat(document.getElementById('c').value);
+   const media = (nota1+nota2+nota3)/3;
+
+   tabela = document.getElementById("tabela");
+
+
+   var linha   = tabela.insertRow(-1);
+
+
+   var coluna1 = linha.insertCell(0);
+   var coluna2 = linha.insertCell(1);
+   var coluna3 = linha.insertCell(2);
+   var coluna4 = linha.insertCell(3);
+   var coluna5 = linha.insertCell(4);
+
+
+   coluna1.innerHTML = nome;
+   coluna2.innerHTML = nota1;
+   coluna3.innerHTML = nota2;
+   coluna4.innerHTML = nota3;
+   coluna5.innerHTML = media;
+
+}
 
 //colocar o campo de entrada e partir dele fazer o calculo(usar validacao)
 function calcularQuantidadePrestacao(){
